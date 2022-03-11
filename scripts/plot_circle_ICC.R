ce371f2181e320b739ba22dff2f60b7ad53d85cb
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-03-11 11:31:27 -0500

    Add discriminability and identification plots

--- scripts/plot_circle_ICC.R
+++ scripts/plot_circle_ICC.R
@@ -4,9 +4,11 @@ library(circlize)
 library(ComplexHeatmap)
 library(tidyr)
 
-#### Dense ####
-dense <- read.csv("results/dense_mean_icc.csv")
+#### Dense Plots ####
+#* ICC ----
+dense <- read.csv("results/mean_icc_dense.csv")
 ndist <- length(unique(dense$Distance))
+dense$Roi <- gsub("ROI", "", dense$Roi)
 
 heat <- NULL
 dists <- c()
@@ -14,7 +16,7 @@ rois <- c()
 for (d in unique(dense$Distance)) {
   df <- dense[dense$Distance == d,]
   heat_sub <- pivot_wider(df[,-1], 
-                      names_from = c("Data_length"), 
+                      names_from = c("Data_length..min."), 
                       values_from = Mean_ICC)
   heat <- rbind(heat, heat_sub[,-1])
   dists <- c(dists, rep(d, nrow(heat_sub)))
@@ -29,7 +31,7 @@ png("figures/dense_ICC_circle.png", units="in", width=5, height=5, res=300)
 circos.clear()
 circos.par(gap.after = c(rep(2, ndist-1), 20))
 
-col_fun <- colorRamp2(c(min(dense$Mean_ICC), max(dense$Mean_ICC)), c("white", "blue"))
+col_fun <- colorRamp2(c(0, 1), c("white", "blue"))
 text(0, 0, 'rownames.side = "inside"')
 circos.heatmap(heat[,10:1], split = as.factor(dists), col = col_fun,
                track.height = 0.4,
@@ -54,17 +56,119 @@ grid.draw(lgd)
 
 dev.off()
 
-#### Sparse ####
-sparse <- read.csv("results/sparse_mean_icc.csv")
-ndist <- length(unique(sparse$Distance))
+#* Discriminability ----
+dense <- read.csv("results/dis_idr_sub.csv")
+ndist <- length(unique(dense$Distance))
 
 heat <- NULL
 dists <- c()
 rois <- c()
-for (d in unique(sparse$Distance)) {
-  df <- sparse[sparse$Distance == d,]
+for (d in unique(dense$Distance)) {
+  df <- dense[dense$Distance == d,]
+  heat_sub <- pivot_wider(df[,2:5], 
+                          names_from = Session, 
+                          values_from = Sub_Discriminability)
+  heat <- rbind(heat, heat_sub[,-1])
+  dists <- c(dists, rep(d, nrow(heat_sub)))
+  rois <- c(rois, heat_sub$ROI)
+}
+heat <- as.matrix(heat)
+rownames(heat) <- rois
+
+# Plot
+png("figures/dense_discrim_circle.png", units="in", width=5, height=5, res=300)
+
+circos.clear()
+circos.par(gap.after = c(rep(2, ndist-1), 20))
+
+col_fun <- colorRamp2(c(0.8, 1), c("white", "blue"))
+text(0, 0, 'rownames.side = "inside"')
+circos.heatmap(heat[,10:1], split = as.factor(dists), col = col_fun,
+               track.height = 0.4,
+               cluster = FALSE,
+               rownames.side = "inside",
+               show.sector.labels = TRUE)
+
+# draw y-axis labels
+circos.track(track.index = get.current.track.index(), panel.fun = function(x, y) {
+  if(CELL_META$sector.numeric.index == ndist) { # the last sector
+    cn = colnames(heat)
+    n = length(cn)
+    circos.text(rep(CELL_META$cell.xlim[2], n) + convert_x(1, "mm"), 
+                1:n - 0.5, cn, 
+                cex = 0.5, adj = c(0, 0.5), facing = "inside")
+  }
+}, bg.border = NA)
+
+# draw legend
+lgd = Legend(title = "Discriminabiltiy", col_fun = col_fun)
+grid.draw(lgd)
+
+dev.off()
+
+#* Identification ----
+dense <- read.csv("results/dis_idr_sub.csv")
+ndist <- length(unique(dense$Distance))
+
+heat <- NULL
+dists <- c()
+rois <- c()
+for (d in unique(dense$Distance)) {
+  df <- dense[dense$Distance == d,]
+  heat_sub <- pivot_wider(df[,c(2:4, 6)], 
+                          names_from = Session, 
+                          values_from = Sub_Identification)
+  heat <- rbind(heat, heat_sub[,-1])
+  dists <- c(dists, rep(d, nrow(heat_sub)))
+  rois <- c(rois, heat_sub$ROI)
+}
+heat <- as.matrix(heat)
+rownames(heat) <- rois
+
+# Plot
+png("figures/dense_identify_circle.png", units="in", width=5, height=5, res=300)
+
+circos.clear()
+circos.par(gap.after = c(rep(2, ndist-1), 20))
+
+col_fun <- colorRamp2(c(0, 1), c("white", "blue"))
+text(0, 0, 'rownames.side = "inside"')
+circos.heatmap(heat[,10:1], split = as.factor(dists), col = col_fun,
+               track.height = 0.4,
+               cluster = FALSE,
+               rownames.side = "inside",
+               show.sector.labels = TRUE)
+
+# draw y-axis labels
+circos.track(track.index = get.current.track.index(), panel.fun = function(x, y) {
+  if(CELL_META$sector.numeric.index == ndist) { # the last sector
+    cn = colnames(heat)
+    n = length(cn)
+    circos.text(rep(CELL_META$cell.xlim[2], n) + convert_x(1, "mm"), 
+                1:n - 0.5, cn, 
+                cex = 0.5, adj = c(0, 0.5), facing = "inside")
+  }
+}, bg.border = NA)
+
+# draw legend
+lgd = Legend(title = "Identifiability", col_fun = col_fun)
+grid.draw(lgd)
+
+dev.off()
+
+#### Sparse Plots ####
+#* ICC ----
+dense <- read.csv("results/mean_icc_sparse.csv")
+ndist <- length(unique(dense$Distance))
+dense$Roi <- gsub("ROI", "", dense$Roi)
+
+heat <- NULL
+dists <- c()
+rois <- c()
+for (d in unique(dense$Distance)) {
+  df <- dense[dense$Distance == d,]
   heat_sub <- pivot_wider(df[,-1], 
-                          names_from = c("Data_length"), 
+                          names_from = c("Data_length..min."), 
                           values_from = Mean_ICC)
   heat <- rbind(heat, heat_sub[,-1])
   dists <- c(dists, rep(d, nrow(heat_sub)))
@@ -100,6 +204,106 @@ circos.track(track.index = get.current.track.index(), panel.fun = function(x, y)
 
 # draw legend
 lgd = Legend(title = "ICC", col_fun = col_fun)
+grid.draw(lgd)
+
+dev.off()
+
+#* Discriminability ----
+dense <- read.csv("results/dis_idr_sub_sparse.csv")
+ndist <- length(unique(dense$Distance))
+
+heat <- NULL
+dists <- c()
+rois <- c()
+for (d in unique(dense$Distance)) {
+  df <- dense[dense$Distance == d,]
+  heat_sub <- pivot_wider(df[,2:5], 
+                          names_from = Session, 
+                          values_from = Sub_Discriminability)
+  heat <- rbind(heat, heat_sub[,-1])
+  dists <- c(dists, rep(d, nrow(heat_sub)))
+  rois <- c(rois, heat_sub$ROI)
+}
+heat <- as.matrix(heat)
+rownames(heat) <- rois
+
+# Plot
+png("figures/sparse_discrim_circle.png", units="in", width=5, height=5, res=300)
+
+circos.clear()
+circos.par(gap.after = c(rep(2, ndist-1), 20))
+
+col_fun <- colorRamp2(c(0.8, 1), c("white", "blue"))
+text(0, 0, 'rownames.side = "inside"')
+circos.heatmap(heat[,10:1], split = as.factor(dists), col = col_fun,
+               track.height = 0.4,
+               cluster = FALSE,
+               rownames.side = "inside",
+               show.sector.labels = TRUE)
+
+# draw y-axis labels
+circos.track(track.index = get.current.track.index(), panel.fun = function(x, y) {
+  if(CELL_META$sector.numeric.index == ndist) { # the last sector
+    cn = colnames(heat)
+    n = length(cn)
+    circos.text(rep(CELL_META$cell.xlim[2], n) + convert_x(1, "mm"), 
+                1:n - 0.5, cn, 
+                cex = 0.5, adj = c(0, 0.5), facing = "inside")
+  }
+}, bg.border = NA)
+
+# draw legend
+lgd = Legend(title = "Discriminabiltiy", col_fun = col_fun)
+grid.draw(lgd)
+
+dev.off()
+
+#* Identification ----
+dense <- read.csv("results/dis_idr_sub_sparse.csv")
+ndist <- length(unique(dense$Distance))
+
+heat <- NULL
+dists <- c()
+rois <- c()
+for (d in unique(dense$Distance)) {
+  df <- dense[dense$Distance == d,]
+  heat_sub <- pivot_wider(df[,c(2:4, 6)], 
+                          names_from = Session, 
+                          values_from = Sub_Identification)
+  heat <- rbind(heat, heat_sub[,-1])
+  dists <- c(dists, rep(d, nrow(heat_sub)))
+  rois <- c(rois, heat_sub$ROI)
+}
+heat <- as.matrix(heat)
+rownames(heat) <- rois
+
+# Plot
+png("figures/sparse_identify_circle.png", units="in", width=5, height=5, res=300)
+
+circos.clear()
+circos.par(gap.after = c(rep(2, ndist-1), 20))
+
+col_fun <- colorRamp2(c(0, 1), c("white", "blue"))
+text(0, 0, 'rownames.side = "inside"')
+circos.heatmap(heat[,10:1], split = as.factor(dists), col = col_fun,
+               track.height = 0.4,
+               cluster = FALSE,
+               rownames.side = "inside",
+               show.sector.labels = TRUE)
+
+# draw y-axis labels
+circos.track(track.index = get.current.track.index(), panel.fun = function(x, y) {
+  if(CELL_META$sector.numeric.index == ndist) { # the last sector
+    cn = colnames(heat)
+    n = length(cn)
+    circos.text(rep(CELL_META$cell.xlim[2], n) + convert_x(1, "mm"), 
+                1:n - 0.5, cn, 
+                cex = 0.5, adj = c(0, 0.5), facing = "inside")
+  }
+}, bg.border = NA)
+
+# draw legend
+lgd = Legend(title = "Identifiability", col_fun = col_fun)
 grid.draw(lgd)
 
 dev.off()
